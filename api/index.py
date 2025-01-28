e1d07ffec88e62cd39bcd3ab310e9c06778e7dcc
ChuishenX <chuishenx@oblivionocean.top>
2025-01-28 19:09:17 +0800

    U

--- api/index.py
+++ api/index.py
@@ -10,6 +10,7 @@ template_root = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="{{ url_for('static', filename='favicon.png') }}" />
     <title>鉴黄</title>
     <style>
         body {
